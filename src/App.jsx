0423b358db6906eac27e89605e38f995e8344d7b
AbelUMA <abelgalvan@uma.es>
2022-10-24 13:04:53 +0200

    store and first reducer prototyped

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,23 @@ import Products from './components/Products'
 import NewProduct from './components/NewProduct'
 import EditProduct from './components/EditProduct'
 import Header from './components/Header'
+//Redux
+import { Provider } from 'react-redux'
+import store from './store'
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Header />
-      <div className="container">
-        <Routes>
-          <Route index path="/" element={<Products />} />
-          <Route path="/products/new" element={<NewProduct />} />
-          <Route path="/products/edit/:id" element={<EditProduct />} />
-        </Routes>
-      </div>
+      <Provider store={store}>
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route index path="/" element={<Products />} />
+            <Route path="/products/new" element={<NewProduct />} />
+            <Route path="/products/edit/:id" element={<EditProduct />} />
+          </Routes>
+        </div>
+      </Provider>
     </BrowserRouter>
   )
 }
